3f1ac6994cb7042eab7dd112185d33bc9c3d8cac
ganesh <ganeshparjaptm5@gmail.com>
2023-02-08 02:14:51 +0530

    Chang logo with other changes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,66 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import { useState } from "react";
 
-import './App.css';
-import Alet from './components/Alet';
-// import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-
+import "./App.css";
+import Alet from "./components/Alet";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 
 function App() {
-  const[mode,setmode]= useState('light');
-  const [alert, setAlert] = useState(null)
+  const [mode, setmode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert =(massage,type) =>{
+  const showAlert = (massage, type) => {
     setAlert({
       msg: massage,
-      type: type
-    })
- 
-  }
+      type: type,
+    });
+  };
   setTimeout(() => {
     setAlert(null);
   }, 1000);
-  
- const togglemode=()=>{
-  if(mode === 'light'){
-   setmode('dark')
-   document.body.style.backgroundColor="#1f1548"
-   showAlert("Dark mode has been enabled" ,"success")
-  }
-  else{
-    setmode('light')
-    document.body.style.backgroundColor="white"
-   showAlert("Light mode has been enabled" ,"success")
 
-  }
-  }
+  const togglemode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#1f1548";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TestUtiles- Dark";
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TestUtiles- Light";
+    }
+  };
   return (
     <>
-      <Navbar 
-      title="TextChange"
-       About="About Us"
-       mode={mode} togglemode={togglemode}
-      />
-       <Alet Alert={alert}/> 
-      <div className='container my-3' >
-      <TextForm showAlert={showAlert}  Heading="Enter the text hear to analyze below " mode={mode}/>
-      {/* <About/> */}
-     
-      </div>
-    </>
-    
+       <Router>
+        <Navbar
+          title="TextChange"
+          About="About Us"
+          mode={mode}
+          togglemode={togglemode}
+        />
+        <Alet Alert={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm
+                showAlert={showAlert} Heading="Enter the text hear to analyze below " mode={mode}/>                  
+            </Route>
+          </Switch>
+        </div>
+        </Router>
+        </>
+  
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,6 +54,25 @@ export default function Navbar(props) {
                 Enable dark mode
               </label>
             </div>
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              } `}
+            >
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                onClick={props.togglemode}
+                id="flexSwitchCheckDefault"
+              />
+              <label
+                className="form-check-label "
+                htmlFor="flexSwitchCheckDefault"
+              >
+                Enable Blue mode
+              </label>
+            </div>
             {/* <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-primary" type="submit">Search</button>
